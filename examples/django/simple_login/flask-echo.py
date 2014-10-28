0d6f7463b2cc565a2a2154e050878c3d2863c727
Alexander Guy <alexander@andern.org>
2014-10-28 12:16:19 -0700

    Implement an external authz checking example.

--- examples/django/simple_login/flask-echo.py
+++ examples/django/simple_login/flask-echo.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 from flask import Flask, request, Response
+import requests
 
 app = Flask(__name__)
 
 @app.route('/echoback')
 def echoback():
+    role_uri = request.headers["X-Tauth-Role-Uri"]
+    authz_url = request.headers["X-Tauth-Authz-Url"]
+
+    r = requests.get(authz_url,
+                     params={
+            'role_uri': role_uri,
+            'action_uri': 'tauth:simple_login:actions:access',
+            'resource_uri': 'tauth:simple_login:resources:echo',
+            })
+
+    if r.status_code != 200:
+        return Response("forbidden", status=403)
+
     res = []
 
     for k, v in request.headers.iteritems():
